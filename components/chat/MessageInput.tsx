33a015b3f87033df810c08f7ce5e655eec831ea5
chrisweb <chris963@gmail.com>
2025-02-07 15:11:10 +0100

    use a submit action instead of a submit handler

--- components/chat/MessageInput.tsx
+++ components/chat/MessageInput.tsx
@@ -3,25 +3,11 @@
 import Form from '@/components/base/Form'
 import Input from '@/components/base/Input'
 import Button from '@/components/base/Button'
+import submitAction from '@/actions/chat/Submit'
 
 const MessageInput: React.FC = () => {
-
-
-    const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-
-        const formData = new FormData(event.currentTarget)
-
-        event.preventDefault()
-
-        // use the entries (names and values) or values (only values)
-        formData.entries().forEach(([name, value], index) => {
-            console.log('name, value, index: ', name, value, index)
-        })
-
-    }
-
     return (
-        <Form onSubmit={onSubmitHandler}>
+        <Form action={submitAction}>
             <Input name="prompt" />
             <Button type="submit">s</Button>
         </Form>
