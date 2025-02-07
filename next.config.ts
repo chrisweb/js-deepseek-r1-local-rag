3f5d788ac09be542c30168782fe3e997e96c66ee
chrisweb <chris963@gmail.com>
2025-02-07 13:22:47 +0100

    add pigment css config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
 import type { NextConfig } from 'next'
-import { withPigment, extendTheme } from '@pigment-css/nextjs-plugin'
-
-const theme = extendTheme({})
+import { withPigment, type PigmentOptions } from '@pigment-css/nextjs-plugin'
+import { createTheme } from '@mui/material'
 
 const nextConfig: NextConfig = {
     /* config options here */
 }
 
-export default withPigment(nextConfig, { theme })
+const pigmentConfig: PigmentOptions = {
+    theme: createTheme({
+        cssVariables: {
+            colorSchemeSelector: 'class',
+        },
+        colorSchemes: { light: true, dark: true },
+        typography: {
+            fontFamily: 'var(--font-roboto)',
+        },
+    }),
+    transformLibraries: ['@mui/material'],
+}
+
+export default withPigment(nextConfig, pigmentConfig)
